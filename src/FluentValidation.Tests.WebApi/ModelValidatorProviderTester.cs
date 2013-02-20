1aba02fafb3379fa4232d8bed86443f50c222efb
Richard Lawley <richard@richardlawley.com>
2013-02-20 17:40:39 +0000

    Unit test additions

--- src/FluentValidation.Tests.WebApi/ModelValidatorProviderTester.cs
+++ src/FluentValidation.Tests.WebApi/ModelValidatorProviderTester.cs
@@ -1,78 +1,91 @@
-﻿namespace FluentValidation.Tests.WebApi
+﻿#region License
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// The latest version of this file can be found at http://www.codeplex.com/FluentValidation
+
+#endregion License
+
+namespace FluentValidation.Tests.WebApi
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Globalization;
-    using System.Linq;
-    using System.Text;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using System.Web.Http.Controllers;
-    using System.Web.Http.Metadata;
-    using System.Web.Http.Metadata.Providers;
-    using System.Web.Http.Validation;
-    using System.Web.Http.Validation.Providers;
-    using FluentValidation.Attributes;
-    using FluentValidation.WebApi;
-    using Moq;
-    using NUnit.Framework;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web.Http.Metadata;
+	using System.Web.Http.Metadata.Providers;
+	using System.Web.Http.Validation;
+	using FluentValidation.Attributes;
+	using FluentValidation.WebApi;
+	using Moq;
+	using NUnit.Framework;
 
-    [TestFixture]
-    public class ModelValidatorProviderTester
-    {
-        public class TestModel { }
-        public class TestModelValidator : AbstractValidator<TestModel> { }
+	[TestFixture]
+	public class ModelValidatorProviderTester
+	{
+		public class TestModel { }
 
-        [Validator(typeof(TestModelValidator2))]
-        public class TestModel2 { }
-        public class TestModelValidator2 : AbstractValidator<TestModel2> { }
+		public class TestModelValidator : AbstractValidator<TestModel> { }
 
-        public class TestModel3 { }
+		[Validator(typeof(TestModelValidator2))]
+		public class TestModel2 { }
 
-        [Test]
-        public void GetValidator_UsingValidatorFactory_ReturnsValidator()
-        {
-            Mock<IValidatorFactory> mockValidatorFactory = new Mock<IValidatorFactory>();
-            mockValidatorFactory.Setup(x => x.GetValidator(typeof(TestModel))).Returns(new TestModelValidator());
+		public class TestModelValidator2 : AbstractValidator<TestModel2> { }
 
-            ModelValidatorProvider provider = new WebApiFluentValidationModelValidatorProvider(mockValidatorFactory.Object);
-            ModelMetadata metadata = CreateMetaData(typeof(TestModel));
+		public class TestModel3 { }
 
-            var results = provider.GetValidators(metadata, Enumerable.Empty<ModelValidatorProvider>());
+		[Test]
+		public void GetValidator_UsingValidatorFactory_ReturnsValidator()
+		{
+			Mock<IValidatorFactory> mockValidatorFactory = new Mock<IValidatorFactory>();
+			mockValidatorFactory.Setup(x => x.GetValidator(typeof(TestModel))).Returns(new TestModelValidator());
 
-            mockValidatorFactory.Verify(x => x.GetValidator(typeof(TestModel)), Times.Once());
-            Assert.AreEqual(1, results.Count());
-            Assert.IsAssignableFrom<WebApiFluentValidationModelValidator<TestModel>>(results.Single());
-        }
+			ModelValidatorProvider provider = new WebApiFluentValidationModelValidatorProvider(mockValidatorFactory.Object);
+			ModelMetadata metadata = CreateMetaData(typeof(TestModel));
 
-        [Test]
-        public void GetValidator_WithoutFactory_ReturnsValidatorFromDefaultFactory()
-        {
-            ModelValidatorProvider provider = new WebApiFluentValidationModelValidatorProvider();
-            ModelMetadata metadata = CreateMetaData(typeof(TestModel2));
+			var results = provider.GetValidators(metadata, Enumerable.Empty<ModelValidatorProvider>());
 
-            IEnumerable<ModelValidator> results = provider.GetValidators(metadata, Enumerable.Empty<ModelValidatorProvider>());
+			mockValidatorFactory.Verify(x => x.GetValidator(typeof(TestModel)), Times.Once());
+			Assert.AreEqual(1, results.Count());
+			Assert.IsAssignableFrom<WebApiFluentValidationModelValidator<TestModel>>(results.Single());
+		}
 
-            Assert.AreEqual(1, results.Count());
-            Assert.IsAssignableFrom<WebApiFluentValidationModelValidator<TestModel2>>(results.Single());
-        }
+		[Test]
+		public void GetValidator_WithoutFactory_ReturnsValidatorFromDefaultFactory()
+		{
+			ModelValidatorProvider provider = new WebApiFluentValidationModelValidatorProvider();
+			ModelMetadata metadata = CreateMetaData(typeof(TestModel2));
 
-        [Test]
-        public void GetValidator_ForUnregisteredValidator_ReturnsNoValidators()
-        {
-            ModelValidatorProvider provider = new WebApiFluentValidationModelValidatorProvider();
-            ModelMetadata metadata = CreateMetaData(typeof(TestModel3));
+			IEnumerable<ModelValidator> results = provider.GetValidators(metadata, Enumerable.Empty<ModelValidatorProvider>());
 
-            IEnumerable<ModelValidator> results = provider.GetValidators(metadata, Enumerable.Empty<ModelValidatorProvider>());
+			Assert.AreEqual(1, results.Count());
+			Assert.IsAssignableFrom<WebApiFluentValidationModelValidator<TestModel2>>(results.Single());
+		}
 
-            Assert.AreEqual(0, results.Count());
-        }
+		[Test]
+		public void GetValidator_ForUnregisteredValidator_ReturnsNoValidators()
+		{
+			ModelValidatorProvider provider = new WebApiFluentValidationModelValidatorProvider();
+			ModelMetadata metadata = CreateMetaData(typeof(TestModel3));
 
+			IEnumerable<ModelValidator> results = provider.GetValidators(metadata, Enumerable.Empty<ModelValidatorProvider>());
 
-        protected ModelMetadata CreateMetaData(Type type)
-        {
-            var meta = new DataAnnotationsModelMetadataProvider();
-            return meta.GetMetadataForType(null, type);
-        }
-    }
+			Assert.AreEqual(0, results.Count());
+		}
+
+		protected ModelMetadata CreateMetaData(Type type)
+		{
+			var meta = new DataAnnotationsModelMetadataProvider();
+			return meta.GetMetadataForType(null, type);
+		}
+	}
 }
--- src/FluentValidation.Tests.WebApi/ModelValidatorTester.cs
+++ src/FluentValidation.Tests.WebApi/ModelValidatorTester.cs
@@ -1,76 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http.Validation;
-using FluentValidation.WebApi;
-using Moq;
-using NUnit.Framework;
+﻿#region License
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// The latest version of this file can be found at http://www.codeplex.com/FluentValidation
+
+#endregion License
 
 namespace FluentValidation.Tests.WebApi
 {
-    [TestFixture]
-    public class ModelValidatorTester
-    {
-        private Mock<IValidatorFactory> _mockFactory;
-        private IEnumerable<ModelValidatorProvider> _modelValidatorProviders;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Web.Http.Metadata;
+	using System.Web.Http.Validation;
+	using FluentValidation.WebApi;
+	using Moq;
+	using NUnit.Framework;
 
-        public class TestModel { }
+	[TestFixture]
+	public class ModelValidatorTester
+	{
+		private Mock<IValidatorFactory> _mockFactory;
+		private IEnumerable<ModelValidatorProvider> _modelValidatorProviders;
 
-        [SetUp]
-        public void SetUp()
-        {
-            _mockFactory = new Mock<IValidatorFactory>();
-            _modelValidatorProviders = Enumerable.Empty<ModelValidatorProvider>();
-        }
+		public class TestModel { }
 
+		[SetUp]
+		public void SetUp()
+		{
+			_mockFactory = new Mock<IValidatorFactory>();
+			_modelValidatorProviders = Enumerable.Empty<ModelValidatorProvider>();
+		}
 
-        [Test]
-        public void Validate_ThrowsException_IfFactoryCannotCreateValidator()
-        {
+		[Test]
+		public void Validate_ThrowsException_IfFactoryCannotCreateValidator()
+		{
+			WebApiFluentValidationModelValidator<TestModel> validator = new WebApiFluentValidationModelValidator<TestModel>(_modelValidatorProviders, _mockFactory.Object);
+			TestModel model = null;
 
-            WebApiFluentValidationModelValidator<TestModel> validator = new WebApiFluentValidationModelValidator<TestModel>(_modelValidatorProviders, _mockFactory.Object);
-            TestModel model = null;
+			Assert.Throws<ArgumentException>(() =>
+			{
+			});
 
-            Assert.Throws<ArgumentException>(() =>
-            {
-                
-            });
+			Assert.Inconclusive();
+		}
 
-            Assert.Inconclusive();
-        }
+		[Test]
+		public void Validate_ReturnsNoResults_WhenSuccessful()
+		{
+			ModelValidator validator = null;
+			ModelMetadata metadata = null;
 
-        [Test]
-        public void Validate_ReturnsNoResults_WhenSuccessful()
-        {
-            Assert.Inconclusive();
-        }
+			var results = validator.Validate(metadata, null);
 
-        [Test]
-        public void Validate_WhenInvalid_ConvertsMessagesToWebApiFormat()
-        {
-            Assert.Inconclusive();
-        }
+			results.Any().ShouldBeFalse();
+		}
 
-        [Test]
-        public void Validate_CreatesValidator_WhenModelIsNotNull()
-        {
-            Assert.Inconclusive();
-        }
+		[Test]
+		public void Validate_WhenInvalid_ConvertsMessagesToWebApiFormat()
+		{
+			Assert.Inconclusive();
+		}
 
-        [Test]
-        public void Validate_CallsValidateOnValidator()
-        {
-            Assert.Inconclusive();
-        }
+		[Test]
+		public void Validate_CreatesValidator_WhenModelIsNotNull()
+		{
+			Assert.Inconclusive();
+		}
 
-        [Test]
-        public void Validate_DoesNotCreateValidator_WhenModelIsNull()
-        {
-            Assert.Inconclusive();
-        }
+		[Test]
+		public void Validate_CallsValidateOnValidator()
+		{
+			Assert.Inconclusive();
+		}
 
+		[Test]
+		public void Validate_DoesNotCreateValidator_WhenModelIsNull()
+		{
+			_mockFactory.Setup(x => x.GetValidator(typeof())
 
-    }
+			Assert.Inconclusive();
+		}
+	}
 }
